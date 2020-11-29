d89ec9fba1b252595a67a4ef1d379aeed249ee1e
Dennis Gosnell <cdep.illabout@gmail.com>
2020-11-29 22:01:08 +0900

    Finish part1 of day1.

--- 2019/racket/day01.rkt
+++ 2019/racket/day01.rkt
@@ -1,11 +1,18 @@
 
 #lang racket
 
+(define (find-fuel mass)
+  (- (quotient mass 3) 2))
+
 (define (main)
   (let* ([in (open-input-file "day01-input")]
          [input-str (port->string in #:close? #t)]
+         [split-input (string-split input-str)]
+         [nums (map string->number split-input)]
+         [fuels (map find-fuel nums)]
+         [sum (apply + fuels)]
          )
-    (print input-str)
+    (print sum)
     )
   )
 
