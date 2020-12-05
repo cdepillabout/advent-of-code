801a4ef3c2efbf8df4d1fdff88af9b3f8676e7b2
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-05 14:18:07 +0900

    2020 Day 04 part 2

--- 2020/racket/day04.rkt
+++ 2020/racket/day04.rkt
@@ -1,12 +1,115 @@
 
 #lang racket
 
+(module+ test
+  (require rackunit)
+  )
+
 (define required-names-set (set "byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"))
+
+(define (validate-num str min max)
+  (let ([num (string->number str)])
+    (and num (<= min num max))
+    )
+  )
+
+(module+ test
+  (check-true (validate-num "100" 50 150))
+  (check-false (validate-num "49" 50 150))
+  (check-true (validate-num "150" 50 150))
+  (check-false (validate-num "151" 50 150))
+  )
+
+(define (is-digit? c)
+  (char<=? #\0 c #\9))
+
+(define (is-all-digit? chars len)
+  (and
+   (andmap is-digit? (string->list chars))
+   (equal? len (length (string->list chars)))
+   )
+  )
+
+(define (is-hex? c)
+  (or (is-digit? c) (char<=? #\a c #\f)))
+
+(define (is-all-hex? chars len)
+  (and
+   (andmap is-hex? (string->list chars))
+   (equal? len (length (string->list chars)))
+   )
+  )
+
+;; byr (Birth Year) - four digits; at least 1920 and at most 2002.
+(define (validate-byr val) (validate-num val 1920 2002))
+
+;; iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+(define (validate-iyr val) (validate-num val 2010 2020))
+
+;; eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+(define (validate-eyr val) (validate-num val 2020 2030))
+
+;; hgt (Height) - a number followed by either cm or in:
+(define (validate-hgt val)
+  ;;   If cm, the number must be at least 150 and at most 193.
+  (define (validate-cm val) (validate-num val 150 193))
+  ;;   If in, the number must be at least 59 and at most 76.
+  (define (validate-in val) (validate-num val 59 76))
+  (let ([is-cm (string-suffix? val "cm")]
+        [is-in (string-suffix? val "in")]
+        [trim-cm (string-trim val "cm" #:left? #f)]
+        [trim-in (string-trim val "in" #:left? #f)]
+        )
+    (cond
+      [is-cm (validate-cm trim-cm)]
+      [is-in (validate-in trim-in)]
+      [else #f]
+      )
+    )
+  )
+
+;; hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+(define (validate-hcl val)
+  (let ([is-leading-hash (string-prefix? val "#")]
+        [trim-hash (string-trim val "#" #:right? #f)]
+        )
+    (and is-leading-hash (is-all-hex? trim-hash 6))
+    )
+  )
+
+;; ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+(define (validate-ecl val)
+  (if (member val '("amb" "blu" "brn" "gry" "grn" "hzl" "oth"))
+      #t
+      #f)
+  )
+
+;; pid (Passport ID) - a nine-digit number, including leading zeroes.
+(define (validate-pid val) (is-all-digit? val 9))
+
+(define (validate-vals names-vals)
+  (define (find-name-val name names-vals)
+    (second (findf (λ(nv)(equal? name (first nv))) names-vals)))
+  (and
+   (validate-byr (find-name-val "byr" names-vals))
+   (validate-iyr (find-name-val "iyr" names-vals))
+   (validate-eyr (find-name-val "eyr" names-vals))
+   (validate-hgt (find-name-val "hgt" names-vals))
+   (validate-hcl (find-name-val "hcl" names-vals))
+   (validate-ecl (find-name-val "ecl" names-vals))
+   (validate-pid (find-name-val "pid" names-vals))
+   )
+  )
 
 (define (contains-required-fields? names-vals)
   (let* ([just-names (map first names-vals)])
-    (subset? required-names-set (list->set just-names))
+    (and
+     (subset? required-names-set (list->set just-names))
+     (validate-vals names-vals)
+     )
   ))
+
+(define my-test '())
 
 (define (is-valid pass)
   (let* ([fields (string-split pass)]
@@ -18,20 +121,21 @@
   )
 
 (define (main)
-  (let* ([in (open-input-file "day04-input")]
+  (let* (
+         [in (open-input-file "day04-input")]
+         ;; [in (open-input-file "day04-input-example")]
          [input-str (port->string in #:close? #t)]
          [split-input (string-split input-str "\n\n")]
          [valids (map is-valid split-input)]
          [valids-count (count identity valids)]
-         ;; [couples (create-couples nums)]
-         ;; [couples-sum (map sum-couple couples)]
-         ;; [filtered-couples (filter is-2020 couples-sum)]
          )
-    (printf "input-str: ~v\n" input-str)
-    (printf "split-input: ~v\n" split-input)
-    (printf "valids: ~v\n" valids)
+    ;; (printf "input-str: ~v\n" input-str)
+    ;; (printf "split-input: ~v\n" split-input)
+    ;; (printf "valids: ~v\n" valids)
     (printf "valids-count: ~v\n" valids-count)
     )
   )
 
-(main)
+;; (module+ main
+  (main)
+  ;; )
