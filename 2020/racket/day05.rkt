16cbced38f97ee82bcdf0f1069c5595e3bbac8fc
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-11 21:45:28 +0900

    2020 Day 05 part 1.

---
+++ 2020/racket/day05.rkt
@@ -0,0 +1,67 @@
+
+#lang racket
+
+(module+ test
+  (require rackunit)
+  )
+
+
+(module+ test
+  ;; (check-equal? (calc-next-col 3 5 3) 1)
+  ;; (check-equal? (calc-next-col 3 5 0) 3)
+  )
+
+(module+ test
+  ;; (check-true (is-tree-here? '((#f #f #t)
+  ;;                              (#f #f #t))
+  ;;                            1 2))
+  )
+
+
+(define/contract (get-seat-id str)
+  (-> string? number?)
+  (let* ([replaces '(("F" "0") ("B" "1") ("R" "1") ("L" "0"))]
+         [binary-str
+          (foldl
+           (λ(reps accum-str) (string-replace accum-str (car reps) (cadr reps)))
+           str
+           replaces
+           )
+          ])
+    ;; (read-binary-str binary-str
+    (read (open-input-string (string-append "#b" binary-str)))
+    )
+  )
+
+
+(module+ test
+  (check-equal? (get-seat-id "FBFBBFFRLR") 357)
+  (check-equal? (get-seat-id "BFFFBBFRRR") 567)
+  (check-equal? (get-seat-id "FFFBBBFRRR") 119)
+  (check-equal? (get-seat-id "BBFFBBFRLL") 820)
+  ;; (check-equal? (calc-next-col 3 5 0) 3)
+  )
+
+
+(define (main)
+  (let* (
+         [in (open-input-file "day05-input")]
+         ;; [in (open-input-file "day03-input-example")]
+         [input-str (port->string in #:close? #t)]
+         [split-input (string-split input-str "\n")]
+         [seat-ids (map get-seat-id split-input)]
+         [max-seat-id (argmax identity seat-ids)]
+         )
+    ;; (printf "input-str: ~v\n" input-str)
+    ;; (printf "split-input: ~v\n" split-input)
+    ;; (printf "tree-line-lists: ~v\n" tree-line-lists)
+    ;; (printf "number-collisions: ~v\n" number-collisions)
+    ;; (printf "collisions-for-pattern: ~v\n" collisions-for-pattern)
+    (printf "max-seat-id: ~v\n" max-seat-id)
+    '()
+    )
+  )
+
+;; (module+ main
+(main)
+;; )
