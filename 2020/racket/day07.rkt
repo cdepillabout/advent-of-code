d64e6f18c9354c31ef9097a43e13920fb9520a6a
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-15 23:28:41 +0900

    Continue to work on Day 07 2020.

--- 2020/racket/day07.rkt
+++ 2020/racket/day07.rkt
@@ -3,7 +3,11 @@
 (require data/either)
 (require data/functor)
 (require data/monad)
+(require graph)
 (require megaparsack megaparsack/text)
+
+;; data/functor clobbers map, so import it as racket-map.
+(require (only-in racket/base (map racket-map)))
 
 (module+ test
   (require rackunit)
@@ -12,6 +16,10 @@
 ;; light red bags contain 1 bright white bag, 2 muted yellow bags.
 ;; bright white bags contain 1 shiny gold bag.
 ;; faded blue bags contain no other bags.
+
+;;;;;;;;;;;;;
+;; Parsing ;;
+;;;;;;;;;;;;;
 
 (define single-color/p
   (map list->string (many+/p letter/p)))
@@ -56,7 +64,8 @@
                 )
   )
 
-(define contained-bags/c (listof (list/c string? number?)))
+(define contained-bag/c (list/c string? number?))
+(define contained-bags/c (listof contained-bag/c))
 
 (define/contract contained-bags/p
   (parser/c char? contained-bags/c)
@@ -116,8 +125,80 @@
                 (success '("faded blue" ())))
   )
 
-(define all-input/p
+(define/contract all-input/p
+  (parser/c char? (listof (list/c string? contained-bags/c)))
   (many+/p contains/p #:sep (string/p "\n")))
+
+;;;;;;;;;;;;;;;;;
+;; Graph Stuff ;;
+;;;;;;;;;;;;;;;;;
+
+;; (graphviz (directed-graph '((a b) (b c) (b x) (x z) (x y) (z zz) (y zz) (d c)) '(10 20 100 999 2000 3000 9999 30)) #:output (open-output-file "day07-example-graph.dot"))
+;;
+;; (dijkstra (directed-graph '((a b) (b c) (b x) (x z) (x y) (z zz) (y zz) (d c)) '(10 20 100 999 2000 3000 9999 30)) 'b)
+;; '#hash((a . +inf.0) (b . 0) (c . 20) (d . +inf.0) (x . 100) (y . 2100) (z . 1099) (zz . 4099))
+;; '#hash((a . #f) (b . #f) (c . b) (d . #f) (x . b) (y . x) (z . x) (zz . z))
+
+(define graph-edge-ends/c (list/c string? string?))
+(define graph-input-item/c (list/c graph-edge-ends/c number?))
+
+(define/contract (build-graph-one-contained container contained)
+  (-> string? contained-bag/c graph-input-item/c)
+  (let ([contained-color (car contained)]
+        [contained-num (cadr contained)]
+        )
+    (list (list contained-color container) contained-num)
+    )
+  )
+
+(module+ test
+  (check-equal? (build-graph-one-contained "light red" '("bright white" 1))
+                '(("bright white" "light red") 1))
+  )
+
+(define/contract (build-graph-input-one-line input)
+  (-> (list/c string? contained-bags/c) (listof graph-input-item/c))
+  (let ([fst-color (car input)]
+        [contained (cadr input)]
+        )
+    (racket-map (λ(one) (build-graph-one-contained fst-color one)) contained)
+    )
+  )
+
+(module+ test
+  (check-equal? (build-graph-input-one-line
+                 '("light red" (("bright white" 1) ("muted yellow" 2))))
+                '((("bright white" "light red") 1)
+                  (("muted yellow" "light red") 2)))
+  (check-equal? (build-graph-input-one-line
+                 '("faded blue" ()))
+                '())
+  )
+
+(define/contract (build-graph input-list)
+  (-> (listof (list/c string? contained-bags/c)) graph?)
+  (let ([input-info (append* (racket-map build-graph-input-one-line input-list))])
+    ;; (printf "jfjfjf: ~v\n" i)
+    (define-values (all-edges all-weights)
+      (for/fold ([edges '()]
+                 [weights '()])
+                ([i input-info])
+        (values (cons (car i) edges) (cons (cadr i) weights))
+        )
+      )
+    (directed-graph all-edges all-weights)
+    )
+  )
+
+;; (dijkstra (directed-graph '((a b) (b c) (b x) (x z) (x y) (z zz) (y zz) (d c)) '(10 20 100 999 2000 3000 9999 30)) 'b)
+;; '#hash((a . +inf.0) (b . 0) (c . 20) (d . +inf.0) (x . 100) (y . 2100) (z . 1099) (zz . 4099))
+;; '#hash((a . #f) (b . #f) (c . b) (d . #f) (x . b) (y . x) (z . x) (zz . z))
+
+(define/contract (how-many-can-contain graph search-color)
+  (-> graph? string? any/c)
+  (define-values (all-connections _) (dijkstra graph search-color))
+  all-connections
+  )
 
 (define (main)
   (let* (
@@ -125,7 +206,8 @@
          [in (open-input-file "day07-input-example")]
          [input-str (port->string in #:close? #t)]
          [input-lines (parse-result! (parse-string all-input/p input-str))]
-         ;; [group-amount (map calc-group-amount groups)]
+         [graph (build-graph input-lines)]
+         [how-many (how-many-can-contain graph "shiny gold")]
          ;; [group-sum (for/sum ((g group-amount)) g)]
          ;; [seat-ids (map get-seat-id split-input)]
          ;; [max-seat-id (argmax identity seat-ids)]
@@ -133,6 +215,9 @@
          )
     ;; (printf "input-str: ~v\n" input-str)
     (printf "input-lines: ~v\n" input-lines)
+    (printf "how-many: ~v\n" how-many)
+    (call-with-output-file "day07-graph.dot" #:exists 'replace
+      (λ (out-file) (graphviz graph #:output out-file)))
     ;; (printf "split-input: ~v\n" split-input)
     ;; (printf "groups: ~v\n" groups)
     ;; (printf "group-amount: ~v\n" group-amount)
