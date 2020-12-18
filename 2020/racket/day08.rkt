ccf0bda75bc3e5b01119a8d7015509d272cbf568
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-18 15:21:34 +0900

    Finally write my correct type.

--- 2020/racket/day08.rkt
+++ 2020/racket/day08.rkt
@@ -12,36 +12,83 @@
 (struct acc ([val : Integer]))
 (struct jmp ([val : Integer]))
 
-(: to-ops (-> (Listof String) Op))
+(: to-ops (-> (List String String) Op))
 (define (to-ops raw-ops)
   ;; (cond
   ;;   [(
-  (match raw-ops
-    [(list "nop" val)
-     (nop (assert (string->number val) exact-integer?))]
-    [(list "acc" val)
-     (acc (assert (string->number val) exact-integer?))]
-    [(list "jmp" val)
-     (jmp (assert (string->number val) exact-integer?))]
-    )
-  )
-
-(: main (-> Null))
-(define (main)
-  (let* (
-         ;; [in (open-input-file "day08-input")]
-         [in (open-input-file "day08-input-example")]
-         [input-str (port->string in #:close? #t)]
-         [lines (string-split input-str "\n")]
-         [instructions (map (λ([x : String])(string-split x " ")) lines)]
-         [ops (map to-ops instructions)]
+  (let* ([op-raw (car raw-ops)]
+         [num-raw (cadr raw-ops)]
+         [num (string->number num-raw)]
          )
-    ;; (printf "input-str: ~v\n" input-str)
-    ;; (printf "lines: ~v\n" lines)
-    (printf "instructions: ~v\n" instructions)
-    (printf "ops: ~v\n" ops)
-    '()
-    )
-  )
+    (if (exact-integer? num)
+        (match op-raw
+          ["nop" (nop num)]
+          ["acc" (acc num)]
+          ["jmp" (jmp num)]
+          )
+        (error "to-ops, num ~v on op ~v not exact integer\n" num op-raw))))
 
-(main)
+(: 2list-inner? (-> (Listof String) Boolean))
+(define (2list-inner? lst)
+  (if (eq? (length lst) 2) #t #f))
+
+;; (: 2list? (-> (Listof String) Boolean : (List String String)))
+;; (define 2list?
+;;   (cast 2list-inner? (-> (Listof String) Boolean : (List String String))))
+
+;; (: 2list? (-> (Listof String) Boolean : #:+ ((List String String) @ 0) #:- Bot))
+;; (: 2list? (-> (Listof String) Boolean : #:+ ((List String String) @ 0) #:- Bot))
+;; (: 2list? (-> (Listof String) Boolean : (List String String)))
+;; (define (2list? lst)
+;;   (if (pair? lst)
+;;       (let ([xxx : (Listof String) (cdr lst)])
+;;         (if (pair? xxx)
+;;             (let ([yyy : (Listof String) (cdr xxx)])
+;;               (if (null? yyy) #t #f))
+;;             #f))
+;;       #f))
+
+;; This works!
+;; (: 2list? (-> (Listof String) Boolean : #:+ Null))
+;; (: 2list? (-> (Listof String) Boolean : #:+ (Null @ 0)))
+;; (: 2list? (-> (Listof String) Boolean : Null))
+;; (define (2list? lst)
+;;   (if (null? lst) #t #f))
+
+;; (: 2list? (All [A] (-> (Listof A) Boolean : (List A))))
+;; (define (2list? lst)
+;;   (if (and (pair? lst)
+;;            ;; (string? (car lst))
+;;            (null? (cdr lst)))
+;;       #t
+;;       #f))
+
+(: 2list? (All [A] (-> (Listof A) Boolean : (List A A))))
+(define (2list? lst)
+  (and (pair? lst) (pair? (cdr lst)) (null? (cddr lst))))
+
+(define-predicate 2list-foo? (List String String))
+
+;; (: main (-> Null))
+;; (define (main)
+;;   (let* (
+;;          ;; [in (open-input-file "day08-input")]
+;;          [in (open-input-file "day08-input-example")]
+;;          [input-str (port->string in #:close? #t)]
+;;          [lines (string-split input-str "\n")]
+;;          [instructions
+;;           (map
+;;            (λ([x : String]) : (List String String) (string-split x " "))
+;;            lines
+;;            )]
+;;          [ops (map to-ops instructions)]
+;;          )
+;;     ;; (printf "input-str: ~v\n" input-str)
+;;     ;; (printf "lines: ~v\n" lines)
+;;     (printf "instructions: ~v\n" instructions)
+;;     (printf "ops: ~v\n" ops)
+;;     '()
+;;     )
+;;   )
+
+;; (main)
