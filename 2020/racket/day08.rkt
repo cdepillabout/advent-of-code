2acc1918f34ef22a8950638ca7e5b5c1902e44a2
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-18 18:38:11 +0900

    2020 Day 08 part 2.

--- 2020/racket/day08.rkt
+++ 2020/racket/day08.rkt
@@ -24,22 +24,72 @@
 (define (2list? lst)
   (and (pair? lst) (pair? (cdr lst)) (null? (cddr lst))))
 
-(: interp (-> (Listof Op) Integer Integer (Setof Integer) Integer))
-(define (interp ops pc accum visited-ops)
-  (let ([curr-op : Op (list-ref ops pc)]
-        [new-visited-ops : (Setof Integer) (set-add visited-ops pc)]
-        )
-    (if (set-member? visited-ops pc)
-        accum
-        (match curr-op
-          [(nop _) (interp ops (add1 pc) accum new-visited-ops)]
-          [(acc amt) (interp ops (add1 pc) (+ accum amt) new-visited-ops)]
-          [(jmp next-op) (interp ops (+ pc next-op) accum new-visited-ops)]
-          ))))
+(: interp (-> (Listof Op)
+              Index
+              Integer
+              Integer
+              (Setof Integer)
+              Boolean
+              (U Integer False)))
+(define (interp ops ops-len pc accum visited-ops should-try-switch)
+  (cond
+    [(set-member? visited-ops pc) #f]
+    [(>= pc ops-len) accum]
+    [else
+     (let ([curr-op : Op (list-ref ops pc)]
+           [new-visited-ops : (Setof Integer) (set-add visited-ops pc)]
+           )
+       (match curr-op
+         [(acc amt)
+          (interp ops ops-len (add1 pc) (+ accum amt)
+                  new-visited-ops should-try-switch)]
 
-(: interpret (-> (Listof Op) Integer))
+         [(nop next-op)
+          (if should-try-switch
+              ;; If this is the first time through, try to switch to instead
+              ;; treating this as a jump and run to the end.
+              (let ([inner-res
+                     (interp ops ops-len (+ pc next-op) accum new-visited-ops #f)])
+                (if inner-res
+                    ;; We got a result!  This was correct op to switch.  Return the
+                    ;; result.
+                    inner-res
+                    ;; We didn't get a result.  This was not the
+                    ;; correct op to switch and we looped.  Try
+                    ;; running this nop normally and switching on the
+                    ;; next one.
+                    (interp ops ops-len (add1 pc) accum new-visited-ops #t)))
+
+              ;; If we've already tried switching a previous op, then
+              ;; don't try again, just run to the end, treating this
+              ;; as a nop
+              (interp ops ops-len (add1 pc) accum new-visited-ops #f))]
+
+         [(jmp next-op)
+          (if should-try-switch
+              ;; If this is the first time through, try to switch to instead
+              ;; treating this as a nop and run to the end.
+              (let ([inner-res
+                     (interp ops ops-len (add1 pc) accum new-visited-ops #f)])
+                (if inner-res
+                    ;; We got a result!  This was correct op to switch.  Return the
+                    ;; result.
+                    inner-res
+                    ;; We didn't get a result.  This was not the
+                    ;; correct op to switch and we looped.  Try
+                    ;; running this jump and switching on the next
+                    ;; one.
+                    (interp ops ops-len (+ pc next-op) accum new-visited-ops #t)))
+
+              ;; If we've already tried switching a previous op, then
+              ;; don't try again, just run to the end, treating this
+              ;; as a jmp
+              (interp ops ops-len (+ pc next-op) accum new-visited-ops #f))]
+         ))]))
+
+(: interpret (-> (Listof Op) (U False Integer)))
 (define (interpret ops)
-  (interp ops 0 0 (set)))
+  (interp ops (length ops) 0 0 (set) #t))
 
 (: main (-> Null))
 (define (main)
@@ -53,7 +103,7 @@
                  (assert (string-split x " ") 2list?))
                lines)]
          [ops : (Listof Op) (map to-ops instructions)]
-         [final-res : Integer (interpret ops)]
+         [final-res : (U False Integer) (interpret ops)]
          )
     ;; (printf "input-str: ~v\n" input-str)
     ;; (printf "lines: ~v\n" lines)
