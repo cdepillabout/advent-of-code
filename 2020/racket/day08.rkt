e1f1037c8093d222dfff5b072cafc5741b5f81b5
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-18 18:10:45 +0900

    2020 Day 08 part 1

--- 2020/racket/day08.rkt
+++ 2020/racket/day08.rkt
@@ -2,14 +2,12 @@
 #lang typed/racket
 
 (define-type Op (U nop acc jmp))
-(struct nop ([val : Integer]))
-(struct acc ([val : Integer]))
-(struct jmp ([val : Integer]))
+(struct nop ([val : Integer]) #:transparent)
+(struct acc ([val : Integer]) #:transparent)
+(struct jmp ([val : Integer]) #:transparent)
 
 (: to-ops (-> (List String String) Op))
 (define (to-ops raw-ops)
-  ;; (cond
-  ;;   [(
   (let* ([op-raw (car raw-ops)]
          [num-raw (cadr raw-ops)]
          [num (string->number num-raw)]
@@ -26,25 +24,42 @@
 (define (2list? lst)
   (and (pair? lst) (pair? (cdr lst)) (null? (cddr lst))))
 
-;; TODO: How to write a default display function for Op???
+(: interp (-> (Listof Op) Integer Integer (Setof Integer) Integer))
+(define (interp ops pc accum visited-ops)
+  (let ([curr-op : Op (list-ref ops pc)]
+        [new-visited-ops : (Setof Integer) (set-add visited-ops pc)]
+        )
+    (if (set-member? visited-ops pc)
+        accum
+        (match curr-op
+          [(nop _) (interp ops (add1 pc) accum new-visited-ops)]
+          [(acc amt) (interp ops (add1 pc) (+ accum amt) new-visited-ops)]
+          [(jmp next-op) (interp ops (+ pc next-op) accum new-visited-ops)]
+          ))))
+
+(: interpret (-> (Listof Op) Integer))
+(define (interpret ops)
+  (interp ops 0 0 (set)))
 
 (: main (-> Null))
 (define (main)
   (let* (
-         ;; [in (open-input-file "day08-input")]
-         [in (open-input-file "day08-input-example")]
+         [in (open-input-file "day08-input")]
+         ;; [in (open-input-file "day08-input-example")]
          [input-str (port->string in #:close? #t)]
          [lines (string-split input-str "\n")]
          [instructions
           (map (λ([x : String]) : (List String String)
                  (assert (string-split x " ") 2list?))
                lines)]
-         [ops (map to-ops instructions)]
+         [ops : (Listof Op) (map to-ops instructions)]
+         [final-res : Integer (interpret ops)]
          )
     ;; (printf "input-str: ~v\n" input-str)
     ;; (printf "lines: ~v\n" lines)
     (printf "instructions: ~v\n" instructions)
-    (printf "ops: ~v\n" ops)
+    (printf "ops: ~a\n" ops)
+    (printf "final-res: ~a\n" final-res)
     '()
     )
   )
