a55c3fe9434f06c6a72d30f12d46c3506e4235f8
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-21 13:59:06 +0900

    Pin nixpkgs in shell.nix

--- 2020/racket/shell.nix
+++ 2020/racket/shell.nix
@@ -1,6 +1,13 @@
 
 # Currently  using nixpkgs-20.09 as of 2020-12-12.
-with import <nixpkgs> {};
+let
+  nixpkgs-src = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/e79793fd859.tar.gz";
+    sha256 = "1a7d1671qdwylxfcgs6hmm1idgsbczvrv2n591qgmba5jrrp07nb";
+  };
+in
+
+with import nixpkgs-src {};
 
 mkShell {
   nativeBuildInputs = [
