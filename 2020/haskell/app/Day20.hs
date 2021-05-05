83249711416d70440e0065a14526f2d307d5e98f
Dennis Gosnell <cdep.illabout@gmail.com>
2021-05-05 16:22:13 +0900

    update

--- 2020/haskell/app/Day20.hs
+++ 2020/haskell/app/Day20.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Data.List ((!!))
 import ClassyPrelude hiding (maximumBy)
+import Data.Coerce (coerce)
 import Data.Foldable (maximumBy, Foldable)
-import Data.Ord      (comparing)
+import Data.Ord (comparing)
+import Data.Vector ((!), generate)
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -20,13 +22,14 @@ data Tile a = Tile TileId a
 data Pixel = PixDot | PixHash
   deriving (Eq, Show)
 
-newtype RawTile = RawTile (Vector (Vector Pixel))
+newtype RawTile p = RawTile (Vector (Vector p))
   deriving (Eq, Show)
 
 newtype SideVal = SideVal Int
   deriving (Eq, Show)
 
-data RawTileWithInfo = RawTileWithInfo RawTile [SideVal]
+data RawTileWithInfo p = RawTileWithInfo (RawTile p) [SideVal]
+  deriving (Eq, Show)
 
 -- |
 -- >>> pixelVecToInt [PixHash, PixDot, PixHash, PixDot]
@@ -73,7 +76,7 @@ rawTileGetBottomFlipped = SideVal . pixelVecToInt . reverse . lastEx
 -- |
 -- >>> rawTileToSideVals $ RawTile $ fromList $ fmap fromList [[PixDot,PixDot,PixHash,PixHash,PixDot,PixHash,PixDot,PixDot,PixHash,PixDot],[PixHash,PixHash,PixDot,PixDot,PixHash,PixDot,PixDot,PixDot,PixDot,PixDot],[PixHash,PixDot,PixDot,PixDot,PixHash,PixHash,PixDot,PixDot,PixHash,PixDot],[PixHash,PixHash,PixHash,PixHash,PixDot,PixHash,PixDot,PixDot,PixDot,PixHash],[PixHash,PixHash,PixDot,PixHash,PixHash,PixDot,PixHash,PixHash,PixHash,PixDot],[PixHash,PixHash,PixDot,PixDot,PixDot,PixHash,PixDot,PixHash,PixHash,PixHash],[PixDot,PixHash,PixDot,PixHash,PixDot,PixHash,PixDot,PixDot,PixHash,PixHash],[PixDot,PixDot,PixHash,PixDot,PixDot,PixDot,PixDot,PixHash,PixDot,PixDot],[PixHash,PixHash,PixHash,PixDot,PixDot,PixDot,PixHash,PixDot,PixHash,PixDot],[PixDot,PixDot,PixHash,PixHash,PixHash,PixDot,PixDot,PixHash,PixHash,PixHash]]
 -- [SideVal 210,SideVal 300,SideVal 498,SideVal 318,SideVal 89,SideVal 616,SideVal 231,SideVal 924]
-rawTileToSideVals :: RawTile -> [SideVal]
+rawTileToSideVals :: RawTile Pixel -> [SideVal]
 rawTileToSideVals (RawTile vecVec) =
   [ rawTileGetTopLeftToRight vecVec
   , rawTileGetTopFlipped vecVec
@@ -97,30 +100,102 @@ parsePixel =
   (char '.' $> PixDot) <|>
   (char '#' $> PixHash)
 
-allPixelsToRawTile :: [[Pixel]] -> RawTile
+allPixelsToRawTile :: [[p]] -> RawTile p
 allPixelsToRawTile = RawTile . fromList . fmap fromList
 
-parseRawTile :: Parser RawTile
+parseRawTile :: Parser (RawTile Pixel)
 parseRawTile = do
   x <- count 10 (count 10 parsePixel <* newline)
   pure $ allPixelsToRawTile x
 
-parseTile :: Parser (Tile RawTile)
+parseTile :: Parser (Tile (RawTile Pixel))
 parseTile = do
   tileId <- parseTileId
   void newline
   rawTile <- parseRawTile
   pure $ Tile tileId rawTile
 
-parseTiles :: Parser [Tile RawTile]
+parseTiles :: Parser [Tile (RawTile Pixel)]
 parseTiles = sepBy parseTile newline
 
-rawTileGetInfo :: RawTile -> RawTileWithInfo
+rawTileGetInfo :: RawTile Pixel -> RawTileWithInfo Pixel
 rawTileGetInfo rawTile = RawTileWithInfo rawTile $ rawTileToSideVals rawTile
 
-solvePuzzle :: [Tile RawTile] -> [[Tile RawTile]]
+rotateAndFlip :: Vector (Vector p) -> [Vector (Vector p)]
+rotateAndFlip v =
+  [ v
+  , rotate90 v
+  , rotate180 v
+  , rotate90 $ rotate180 v
+  , flipMat v
+  , rotate90 $ flipMat v
+  , rotate180 $ flipMat v
+  , rotate90 $ rotate180 $ flipMat v
+  ]
+
+ll2vv :: [[a]] -> Vector (Vector a)
+ll2vv = fromList . fmap fromList
+
+-- |
+-- >>> let raw1 = ll2vv [[1,2,3],[4,5,6]]
+-- >>> let tile1 = Tile (TileId 1) $ RawTileWithInfo (RawTile raw1) []
+-- >>> let raw2 = ll2vv [[7,8,9],[10,11,12]]
+-- >>> let tile2 = Tile (TileId 2) $ RawTileWithInfo (RawTile raw2) []
+-- >>> let raw3 = ll2vv [[13,14,15],[16,17,18]]
+-- >>> let tile3 = Tile (TileId 3) $ RawTileWithInfo (RawTile raw3) []
+-- >>> let total = pickPiece [tile1, tile2, tile3]
+-- >>> length total
+-- 24
+pickPiece
+  :: forall p
+   . [Tile (RawTileWithInfo p)]
+  -> [(Tile (RawTileWithInfo p), [Tile (RawTileWithInfo p)])]
+pickPiece [] = []
+pickPiece (h@(Tile tileId (RawTileWithInfo rawTile sideVals)) : ts) =
+  fmap
+    (\v -> (Tile tileId (RawTileWithInfo (RawTile v) sideVals), ts))
+    (rotateAndFlip (coerce rawTile))
+  <>
+  fmap (\(a, x) -> (a, h:x)) (pickPiece ts)
+
+-- |
+-- >>> rotate90 $ ll2vv [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+-- [[4,8,12],[3,7,11],[2,6,10],[1,5,9]]
+rotate90 :: forall a. Vector (Vector a) -> Vector (Vector a)
+rotate90 v =
+  let outerLen = length v
+      innerLen = length $ headEx v
+
+      f :: Int -> Vector a
+      f row = -- value 0 to 3
+        let g :: Int -> a
+            g col = -- value 0 to 2
+              (v ! col :: Vector a) ! (innerLen - row - 1)  :: a
+        in generate outerLen g
+  in generate innerLen f
+
+-- |
+-- >>> rotate180 $ fromList $ fmap fromList [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+-- [[12,11,10,9],[8,7,6,5],[4,3,2,1]]
+rotate180 :: forall a. Vector (Vector a) -> Vector (Vector a)
+rotate180 = rotate90 . rotate90
+
+-- |
+-- >>> flipMat $ ll2vv [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+-- [[4,3,2,1],[8,7,6,5],[12,11,10,9]]
+flipMat :: forall a. Vector (Vector a) -> Vector (Vector a)
+flipMat = fmap reverse
+
+findTopRow
+  :: [Tile (RawTileWithInfo Pixel)] -> [([Tile (RawTileWithInfo Pixel)], [Tile (RawTileWithInfo Pixel)])]
+findTopRow inputTiles = do
+  (piece, remainingPieces) <- pickPiece inputTiles
+  undefined
+
+
+solvePuzzle :: [Tile (RawTile Pixel)] -> [[Tile (RawTile Pixel)]]
 solvePuzzle inputTiles@(Tile id r@(RawTile vecVec) : ts) =
-  let inputTilesWithSideVals = fmap (fmap rawTileGetInfo) inputTiles :: [Tile RawTileWithInfo]
+  let inputTilesWithSideVals = fmap (fmap rawTileGetInfo) inputTiles :: [Tile (RawTileWithInfo Pixel)]
       howManyOnSide = sqrt $ fromIntegral $ length inputTiles :: Float
   in
   traceShow howManyOnSide $ traceShow r $ traceShow (rawTileToSideVals r) undefined
