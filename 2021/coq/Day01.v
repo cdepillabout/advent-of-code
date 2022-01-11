4adc100510d1c9dc5578b939a507cc6e89373869
Dennis Gosnell <cdep.illabout@gmail.com>
2022-01-11 20:53:09 +0900

    Get Day01 part01 working in Coq.

--- 2021/coq/Day01.v
+++ 2021/coq/Day01.v
@@ -92,90 +92,52 @@ Fixpoint count_increases (l : list N): N :=
       end
   | _ => 0
   end.
-  
-Theorem one_le_n_pos : forall p, 1%N <= N.pos p.
-Proof.
-  intro p.
-  unfold N.le.
-  unfold not.
-  Print N.compare.
-  simpl.
-  Admitted.
-  (*
-  induction p.
-  - destruct (1 ?= p~1)%positive eqn:E.
-    + admit.
-    + admit.
-    + 
-  - intros. discriminate H.
-  - intros.
-  Print N.le.
-  *)
 
 Example count_increases_example :
   count_increases [199%N; 200%N; 208%N; 210%N; 200%N; 207%N; 240%N; 269%N; 260%N; 263%N] = 7.
 Proof. reflexivity. Qed.
 
-Theorem y_minus_x_is_pos : forall (y x : N) (p : positive), y - x = N.pos p -> exists z, y = N.pos z.
-Proof.
-  induction y.
-  - simpl. intros. discriminate H.
-  - intros.
-  
-
-
 Theorem count_increases_adds_one_for_increase :
   forall x y rest, x < y -> 1 + count_increases (y :: rest) = count_increases (x :: y :: rest).
 Proof.
-  (*
-  intros x y rest.
-  generalize dependent x.
-  generalize dependent y.
-  induction rest.
-  - intros y x HXltY. assert (1 <= y - x).
-    { admit. }
-    Print reflect.
-    Check N.leb_spec0.
-    set (G := N.leb_spec0 1%N (y - x)).
-    inversion G.
-    * simpl. rewrite <- H0. reflexivity.
-    * apply H1 in H. destruct H.
-  -
-  *) 
-  (*intros x y.
-  generalize dependent x.
-  induction y as [|y'].
-  - admit.
-  - *)
-  (*
-  intros x y rest H.
-  destruct x,y,rest; try (inversion H).
-  - simpl.
-  *)
   intros x y rest H.
   unfold count_increases at 2.
   set (G := N.leb_spec0 1%N (y - x)).
   inversion G.
   - reflexivity.
   - fold count_increases.
-    exfalso. apply H1.
-    clear H0 H1 G.
-    unfold N.le.
-    unfold not.
-    intro H1.
-    unfold N.compare in H1.
-    unfold N.lt in H.
-    unfold N.compare in H.
-    destruct (y - x) eqn:E.
-    + destruct x.
-      * rewrite N.sub_0_r in E. rewrite E in H. discriminate H.
-      * clear H1.
-        destruct y.
-        -- discriminate H.
-        -- apply N.sub_0_le in E. unfold N.le in E. unfold not in E.
-           apply E. apply N.lt_gt. apply H.
-    + 
+    clear G H1.
+    exfalso.
+    symmetry in H0.
+    apply N.leb_gt in H0.
+    apply N.lt_1_r in H0.
+    apply N.sub_gt in H.
+    apply H. assumption.
+  Qed.
   
+Theorem lt_is_minus_zero : forall (a b : N), a <= b -> a - b = 0.
+Proof.
+  induction a.
+  - simpl. reflexivity.
+  - induction p.
+    + simpl. Admitted.
+  
+Theorem count_increases_same_for_same :
+  forall x y rest, y <= x -> count_increases (y :: rest) = count_increases (x :: y :: rest).
+Proof.
+  intros x y rest H.
+  unfold count_increases at 2.
+  set (G := N.leb_spec0 1%N (y - x)).
+  inversion G.
+  - fold count_increases.
+    clear G H0.
+    exfalso.
+    apply N.sub_0_le in H.
+    rewrite H in H1.
+    apply (N.nle_succ_0 0).
+    simpl. assumption.
+  - reflexivity.
+  Qed.
 
 Existing Instance Functor_option.
 
